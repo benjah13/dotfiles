4a12c118fbbae8a1e12b707da3effe018b4a822e
benjamin.buleon <buleon.benjamin@gmail.com>
2025-07-17 20:16:51 +0200

    feat(nvim) add spectre

---
+++ nvim/lua/plugins/spectre.lua
@@ -0,0 +1,18 @@
+return { 
+ 'nvim-pack/nvim-spectre',
+  config = function()
+    require('spectre').setup({
+      live_update = true
+    })
+    -- Map Ctrl+Shift+F to toggle Spectre
+    vim.keymap.set('n', '<D-S-f>', '<cmd>lua require("spectre").toggle()<CR>', {
+      desc = "Toggle Spectre (Global Search & Replace)"
+    })
+
+    -- Optional: Also map it in visual mode to search selected text
+    vim.keymap.set('v', '<D-S-f>', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+      desc = "Search selected text with Spectre"
+    })
+ end
+}
+
